d4c8fad40362024c120f563a9c772e0b1104c2ab
MSpilari <mb.spilari@hotmail.com>
2020-10-02 16:26:18 -0300

    Alterando a foto principal

--- src/components/Apresentation/index.jsx
+++ src/components/Apresentation/index.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import MyImg from '../../assets/foto.png'
+import MyImg from '../../assets/NovaFoto.png'
 import './HelloPage.css'
 
 function StartPart() {
-  return(
-    <div className='containerHello'>
-      <div className='texts'>
-        <p>Olá, eu sou o</p>
-        <p>Matheus B.<br/>Spilari</p>
-        <p>Front-End Developer</p>
-      </div>
-      <div className='photo'>
-        <img src={MyImg} alt="Foto"/>
-      </div>
-    </div>
-  );
+	return (
+		<div className='containerHello'>
+			<div className='texts'>
+				<p>Olá, eu sou o</p>
+				<p>
+					Matheus B.
+					<br />
+					Spilari
+				</p>
+				<p>Front-End Developer</p>
+			</div>
+			<div className='photo'>
+				<img src={MyImg} alt='Foto' />
+			</div>
+		</div>
+	)
 }
 
-export default StartPart;
+export default StartPart
